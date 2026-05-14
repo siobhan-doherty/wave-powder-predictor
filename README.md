53c2f8511fd01aacb1a4dcc4fb6f4051ed552e88
siobhan-doherty <siobhan-doherty@users.noreply.github.com>
2026-05-14 10:34:10 +0100

    initial wave-powder-predictor setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Wave Powder Predictor
+
+Surf + Snow conditions
+
+## Quick start
+
+```bash
+git clone https://github.com/siobhan-doherty/wave-powder-predictor
+cd wave-powder-predictor
+docker compose up
---
+++ data/fetch_conditions.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os 
+import requests
+
+
+load_dotenv()
+
+def fetch_wave_forecase(latitude, longitude):
+    # magicseaweed api placeholder
+    pass
+
+def fetch_snow_forecase(latitude, longitude):
+    # openweathermap placeholder
+    pass
